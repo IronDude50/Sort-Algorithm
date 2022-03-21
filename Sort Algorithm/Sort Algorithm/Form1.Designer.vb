ba3d71bd97737bb5f7d65231ab1ab673ff971c0b
IronDude50 <101145918+IronDude50@users.noreply.github.com>
2022-03-21 21:17:50 +1100

    Add files via upload

---
+++ Sort Algorithm/Sort Algorithm/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.rbn100 = New System.Windows.Forms.RadioButton()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbn500 = New System.Windows.Forms.RadioButton()
+        Me.rbn5000 = New System.Windows.Forms.RadioButton()
+        Me.rbn1000 = New System.Windows.Forms.RadioButton()
+        Me.rbnSelectionSort = New System.Windows.Forms.RadioButton()
+        Me.rbnQuickSort = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.lblComment = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(12, 224)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(106, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'rbn100
+        '
+        Me.rbn100.AutoSize = True
+        Me.rbn100.Location = New System.Drawing.Point(6, 22)
+        Me.rbn100.Name = "rbn100"
+        Me.rbn100.Size = New System.Drawing.Size(43, 19)
+        Me.rbn100.TabIndex = 1
+        Me.rbn100.TabStop = True
+        Me.rbn100.Text = "100"
+        Me.rbn100.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(124, 12)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(390, 242)
+        Me.txtOutput.TabIndex = 2
+        Me.txtOutput.Text = "Output"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rbn100)
+        Me.GroupBox1.Controls.Add(Me.rbn500)
+        Me.GroupBox1.Controls.Add(Me.rbn5000)
+        Me.GroupBox1.Controls.Add(Me.rbn1000)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(106, 125)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sample Size"
+        '
+        'rbn500
+        '
+        Me.rbn500.AutoSize = True
+        Me.rbn500.Location = New System.Drawing.Point(6, 47)
+        Me.rbn500.Name = "rbn500"
+        Me.rbn500.Size = New System.Drawing.Size(43, 19)
+        Me.rbn500.TabIndex = 4
+        Me.rbn500.TabStop = True
+        Me.rbn500.Text = "500"
+        Me.rbn500.UseVisualStyleBackColor = True
+        '
+        'rbn5000
+        '
+        Me.rbn5000.AutoSize = True
+        Me.rbn5000.Location = New System.Drawing.Point(6, 97)
+        Me.rbn5000.Name = "rbn5000"
+        Me.rbn5000.Size = New System.Drawing.Size(49, 19)
+        Me.rbn5000.TabIndex = 6
+        Me.rbn5000.TabStop = True
+        Me.rbn5000.Text = "5000"
+        Me.rbn5000.UseVisualStyleBackColor = True
+        '
+        'rbn1000
+        '
+        Me.rbn1000.AutoSize = True
+        Me.rbn1000.Location = New System.Drawing.Point(6, 72)
+        Me.rbn1000.Name = "rbn1000"
+        Me.rbn1000.Size = New System.Drawing.Size(49, 19)
+        Me.rbn1000.TabIndex = 5
+        Me.rbn1000.TabStop = True
+        Me.rbn1000.Text = "1000"
+        Me.rbn1000.UseVisualStyleBackColor = True
+        '
+        'rbnSelectionSort
+        '
+        Me.rbnSelectionSort.AutoSize = True
+        Me.rbnSelectionSort.Location = New System.Drawing.Point(6, 22)
+        Me.rbnSelectionSort.Name = "rbnSelectionSort"
+        Me.rbnSelectionSort.Size = New System.Drawing.Size(97, 19)
+        Me.rbnSelectionSort.TabIndex = 7
+        Me.rbnSelectionSort.TabStop = True
+        Me.rbnSelectionSort.Text = "Selection Sort"
+        Me.rbnSelectionSort.UseVisualStyleBackColor = True
+        '
+        'rbnQuickSort
+        '
+        Me.rbnQuickSort.AutoSize = True
+        Me.rbnQuickSort.Location = New System.Drawing.Point(6, 47)
+        Me.rbnQuickSort.Name = "rbnQuickSort"
+        Me.rbnQuickSort.Size = New System.Drawing.Size(80, 19)
+        Me.rbnQuickSort.TabIndex = 8
+        Me.rbnQuickSort.TabStop = True
+        Me.rbnQuickSort.Text = "Quick Sort"
+        Me.rbnQuickSort.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rbnSelectionSort)
+        Me.GroupBox2.Controls.Add(Me.rbnQuickSort)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 143)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(106, 75)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Sort Style"
+        '
+        'btnSort
+        '
+        Me.btnSort.Enabled = False
+        Me.btnSort.Location = New System.Drawing.Point(12, 253)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(106, 23)
+        Me.btnSort.TabIndex = 8
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'lblComment
+        '
+        Me.lblComment.AutoSize = True
+        Me.lblComment.Location = New System.Drawing.Point(124, 257)
+        Me.lblComment.Name = "lblComment"
+        Me.lblComment.Size = New System.Drawing.Size(74, 15)
+        Me.lblComment.TabIndex = 9
+        Me.lblComment.Text = "lblComment"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(527, 290)
+        Me.Controls.Add(Me.lblComment)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents rbn100 As RadioButton
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rbn500 As RadioButton
+    Friend WithEvents rbn5000 As RadioButton
+    Friend WithEvents rbn1000 As RadioButton
+    Friend WithEvents rbnSelectionSort As RadioButton
+    Friend WithEvents rbnQuickSort As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnSort As Button
+    Friend WithEvents lblComment As Label
+End Class
---
+++ Sort Algorithm/Sort Algorithm/Form1.vb
@@ -0,0 +1,121 @@
+﻿' Sorting Algorithm
+' Dhruv Menon
+' Version 1.0
+' Completed on 10.03.2022
+
+Public Class Form1
+    Dim arrNumbers() As Integer
+    Dim intSwapCount As Int16 = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rbn100.Checked = True
+        rbnSelectionSort.Checked = True
+        lblComment.Text = ""
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Randomize()
+        Dim intSampleSize As UInt16
+        txtOutput.Clear()
+        intSwapCount = 0
+
+        If rbn100.Checked = True Then intSampleSize = 100
+        If rbn500.Checked = True Then intSampleSize = 500
+        If rbn1000.Checked = True Then intSampleSize = 1000
+        If rbn5000.Checked = True Then intSampleSize = 5000
+
+        ReDim arrNumbers(intSampleSize - 1)
+        For i = 0 To arrNumbers.Count - 1
+            arrNumbers(i) = CInt(Int(Rnd() * intSampleSize))
+            txtOutput.Text += Str(arrNumbers(i))
+        Next
+
+        lblComment.Text = "Numbers generated."
+        btnSort.Enabled = True
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        txtOutput.Clear()
+
+        If rbnSelectionSort.Checked = True Then
+            SelectionSort()
+        ElseIf rbnQuickSort.Checked = True Then
+            QuickSort(arrNumbers, 0, arrNumbers.Count - 1)
+        End If
+
+        For i = 0 To arrNumbers.Count - 1
+            txtOutput.Text += Str(arrNumbers(i))
+        Next
+
+        lblComment.Text = intSwapCount.ToString & " swaps performed."
+        btnSort.Enabled = False
+    End Sub
+
+    Private Sub SelectionSort()
+        ' The selection sort algorithm sorts an array by repeatedly finding the 
+        ' minimum element(considering ascending order) from unsorted part and 
+        ' putting it at the beginning. The algorithm maintains two subarrays 
+        ' In a given array.
+        ' 1) The subarray which Is already sorted.
+        ' 2) Remaining subarray which Is unsorted.
+
+        Dim minDex, intLoop1, intLoop2 As Int32
+
+        For intLoop2 = 0 To arrNumbers.Count - 2
+            minDex = intLoop1
+            For intLoop1 = intLoop2 + 1 To arrNumbers.Count - 2
+                If arrNumbers(intLoop1) < arrNumbers(minDex) Then minDex = intLoop1
+            Next
+            Swap(minDex, intLoop2)
+        Next
+    End Sub
+
+    Private Sub QuickSort(intSubList() As Int32, ByVal intFirst As Int16, ByVal intLast As Int16)
+        '   Quick sort algorithm (divide and conquere in-place) recursively 
+        '   1. Choose any element in the array array[p..r]. Call this element the pivot q
+        '   2. Rearrange the elements in array[p..r] so that all elements in array[p..r] 
+        '   that are less than Or equal (<=) To the pivot q are To its left And all 
+        '   elements that are greater (<) than the pivot q are To its right
+        '   3. call quicksort on subarray array[p..q-1] and call quicksort on subarray 
+        '   array[q+1..r] Note item q Is Not part of either of these subarrays 
+        '   4. these subarrays are then joined together recursively as each call 
+        '   to quicksort ends
+
+        Dim Low, High As Int16
+        Dim Pivot As String
+
+        Low = intFirst
+        High = intLast
+        Pivot = intSubList((intFirst + intLast) \ 2)
+
+        Do
+            While intSubList(Low) < Pivot
+                Low = Low + 1
+            End While
+
+            While intSubList(High) > Pivot
+                High = High - 1
+            End While
+
+            ' intCompCount += 1
+            If Low <= High Then
+                Swap(Low, High)
+                Low = Low + 1
+                High = High - 1
+            End If
+        Loop While Low <= High
+
+        If intFirst < High Then QuickSort(intSubList, intFirst, High)
+        If Low < intLast Then QuickSort(intSubList, Low, intLast)
+    End Sub
+
+    Sub Swap(intNdxA As Int32, intNdxB As Int32)
+        Dim intTemp As Int32
+
+        intTemp = arrNumbers(intNdxA)
+        arrNumbers(intNdxA) = arrNumbers(intNdxB)
+        arrNumbers(intNdxB) = intTemp
+
+        intSwapCount += 1
+    End Sub
+End Class
